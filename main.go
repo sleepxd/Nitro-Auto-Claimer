013be4c9dc244e30cee0f42f97be6a9581bdc35a
sleepxd <47408082+sleepxd@users.noreply.github.com>
2020-01-10 16:06:47 -0500

    Update main.go

--- main.go
+++ main.go
@@ -9,18 +9,27 @@ import (
 	"net/http"
 	"io/ioutil"
 	"log"
+	"bytes"
 	"strings"
-
+	"encoding/json"
+	"time"
   	"github.com/gookit/color"
 	"github.com/bwmarrin/discordgo"
+	"github.com/denisbrodbeck/machineid"
 )
 
 
 var T,_ = ioutil.ReadFile("token.txt")
 var Token string = strings.Replace(string(T), "\n", "", 1)
 
+var Redeemed []string
+
 func main() {
   fmt.Println("Developed by sleep")
+  id, err := machineid.ProtectedID("claimer")
+  if err != nil {
+    log.Fatal(err)
+  }
   dg, err := discordgo.New(Token)
 	_, err = dg.User("@me")
 	if err != nil {
@@ -43,9 +52,11 @@ func main() {
 	dg.Close()
 }
 
-func redeem_code(code string) {
+func redeem_code(code string, channel_id string) {
 		client := &http.Client{}
-		req, err := http.NewRequest("POST", "https://discordapp.com/api/v6/entitlements/gift-codes/"+code+"/redeem", nil)
+		values := map[string]string{"channel_id": channel_id, "payment_source_id": "123"}
+		jsonValue, _ := json.Marshal(values)
+		req, err := http.NewRequest("POST", "https://discordapp.com/api/v6/entitlements/gift-codes/"+code+"/redeem", bytes.NewBuffer(jsonValue))
 		req.Header.Add("content-type", "application/json")
 		req.Header.Add("Authorization", Token)
 		resp, err := client.Do(req)
@@ -55,16 +66,17 @@ func redeem_code(code string) {
     if err != nil {
         log.Fatal(err)
     }
-    /*bodyString := string(bodyBytes)
-		return bodyString*/
+	//bodyString := string(bodyBytes)
+	//fmt.Println(bodyString)
 		switch status := resp.StatusCode; status {
 		case 200:
 			color.Success.Println("Redeemed Nitro: " + code)
 		case 404:
 			color.Danger.Println("Invalid Code: " + code)
 		case 400:
-			color.Comment.Println("Code Already Used: " + code)
+			color.Danger.Println("Invalid Code: " + code)
 		}
+		Redeemed = append(Redeemed, code)
 }
 
 func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
@@ -73,11 +85,25 @@ func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
 		return
 	}
 
-  match1, _ := regexp.MatchString("(?:https?:)?discord.gift.(\\S+)", m.Content)
+  match1, _ := regexp.MatchString("(?:https?:)?discord(?:app.com/gifts/|.gift/)([^\\s]+)", m.Content)
   if match1 == true {
-    re := regexp.MustCompile("(?:https?:)?discord.gift.(\\S+)")
+    re := regexp.MustCompile("(?:https?:)?discord(?:app.com/gifts/|.gift/)([^\\s]+)")
     match := re.FindStringSubmatch(m.Content)
-    code := match[1]
-    redeem_code(code)
+	code := match[1]
+	//fmt.println(m.channelID)
+	_, found := Find(Redeemed, code)
+	if !found && len(code) == 16 || len(code) == 24 {
+		redeem_code(code, m.ChannelID)
+	} else {
+		fmt.Println(code, "- Already Attempted or Invalid Format")
+	}
 }
+}
+func Find(slice []string, val string) (int, bool) {
+    for i, item := range slice {
+        if item == val {
+            return i, true
+        }
+    }
+    return -1, false
 }
